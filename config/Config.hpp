5cf14a3218efb0aa3028730c2f1f69422e8e54fb
kyu-baek <rbfla6334@gmail.com>
2022-12-12 18:49:13 +0900

    UPDATE: simple parse #3

--- config/Config.hpp
+++ config/Config.hpp
@@ -11,6 +11,12 @@
 #include "Config_struct.hpp"
 // using namespace std;
 
+
+/*	Request class
+	author	:kbaek
+	date 	:2022.12.10
+*/
+
 class BaseServer
 {
 public:
@@ -64,9 +70,14 @@ public:
 	Config &operator=(Config const &srcs);
 	~Config();
 	Config(const char* _path);
+	Config init(const char* _path);
 	void tokenize();
 	void parse();
 	std::string buff;
+
+	class	FileNotFoundException: public std::exception{
+		virtual const char	*what() const throw();
+	};
 };
 
 std::ostream &operator<<(std::ostream &ost, const Config &conf);
@@ -86,5 +97,41 @@ Config::Config(const char* _path)
 
 }
 
+Config::Config(const char*	_path) {
 
+	try {
+		Config::init(_path);
+	}
+	catch (Config::FileNotFoundException &e) {
+		std::cerr <<  "Error: couldn't open default config file" << std::endl;
+	}
+	return ;
+}
+
+File	init(const char *filename) {
+	int							ret = READER_BUFFER_SIZE;
+	char						buffer[READER_BUFFER_SIZE + 1];
+	std::string					line = "";
+	int							fd;
+	File					file;
+
+	for (int i = 0; i < READER_BUFFER_SIZE + 1;  i++)
+		buffer[i] = '\0';
+	if ((fd = open(filename, O_RDONLY)) <= 0)
+		throw ConfigReader::FileNotFoundException();
+
+	for (ret = READER_BUFFER_SIZE; ret > 0;\
+	ret = read(fd, buffer,READER_BUFFER_SIZE )) {
+		buffer[ret] = '\0';
+		line += buffer;
+	}
+
+	//	Get the last line, which is ignored otherwise
+	if (ret == -1) {
+		std::cerr << "Error: couldn't reading config file" <<std::endl;
+		return file;
+	}
+	file = ConfigReader::split(line, std::string(" \n\t"));
+ 	return file;
+}
 #endif
