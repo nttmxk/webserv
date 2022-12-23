8114954a0280a780d8a0100c680c60a9d163784f
kyu-baek <rbfla6334@gmail.com>
2022-12-23 18:27:42 +0900

    UPDATE: config parse #3

--- config/Config.hpp
+++ config/Config.hpp
@@ -7,6 +7,7 @@
 # include <stdexcept>
 # include <vector>
 # include <map>
+#include <stdio.h>
 
 #include "Config_struct.hpp"
 // using namespace std;
@@ -22,17 +23,17 @@ class BaseServer
 public:
 
     BaseServer(){};
-	BaseServer(const std::string& src);
+	BaseServer(const std::string& src) {};
     ~BaseServer() {};
     
     //  Access Location
-	Location& operator[](const std::string& key);
-	const std::map<std::string, Location>& getLocations() const;
+	// Location& operator[](const std::string& key);
+	// const std::map<std::string, Location>& getLocations() const;
 
-    //  Access Attribution
-	const std::vector<std::string>& BServer(const std::string& key) const;
+    // //  Access Attribution
+	// const std::vector<std::string>& BServer(const std::string& key) const;
 
-	bool is_exist(const std::string& key, std::string) const;
+	// bool is_exist(const std::string& key, std::string) const;
 	//	data to std::string
 	// std::string id();
 	// std::string str(size_t tab_size);
@@ -57,49 +58,50 @@ protected:
 
 class Config : public Config_base
 {
+public:
+	Config();
+	Config(Config const &src);
+	// Config &operator=(Config const &srcs) {};
+	~Config();
+	Config(const std::string path);
+
+	class	FileNotFoundException: public std::exception{
+		virtual const char	*what() const throw();
+	};
+
+
+private:
+	void configInit(const std::string path);
+	void configParse();
+	// void tokenize();
+	// void parse();
+
+
 private:
 	// std::string path;
 	// std::string content;
 	// std::string token;
 	// int fd;
 	// int workers;
-
-public:
-	Config();
-	Config(Config const &src);
-	Config &operator=(Config const &srcs);
-	~Config();
-	Config(const char* _path);
-	Config init(const char* _path);
-	void tokenize();
-	void parse();
-	std::string buff;
-
-	class	FileNotFoundException: public std::exception{
-		virtual const char	*what() const throw();
-	};
+	std::vector<std::string> file;
 };
 
-std::ostream &operator<<(std::ostream &ost, const Config &conf);
+// std::ostream &operator<<(std::ostream &ost, const Config &conf);
 
 Config::Config()
 {}
 
 Config::~Config()
-{
-}
+{}
 
+Config::Config(Config const &src)
+{}
 
-Config::Config(const char* _path)
-{
-	
-
-}
-
-Config::Config(const char*	_path) {
+Config::Config(const std::string _path) {
 
 	try {
-		Config::init(_path);
+		Config::configInit(_path);
+		Config::configParse();
 	}
 	catch (Config::FileNotFoundException &e) {
 		std::cerr <<  "Error: couldn't open default config file" << std::endl;
@@ -107,30 +109,60 @@ Config::Config(const char*	_path) {
 	return ;
 }
 
-File	init(const char *filename) {
-	int							ret = READER_BUFFER_SIZE;
-	char						buffer[READER_BUFFER_SIZE + 1];
-	std::string					line = "";
-	int							fd;
-	File					file;
+void	Config::configInit(const std::string _path) {
 
-	for (int i = 0; i < READER_BUFFER_SIZE + 1;  i++)
-		buffer[i] = '\0';
-	if ((fd = open(filename, O_RDONLY)) <= 0)
-		throw ConfigReader::FileNotFoundException();
 
-	for (ret = READER_BUFFER_SIZE; ret > 0;\
-	ret = read(fd, buffer,READER_BUFFER_SIZE )) {
-		buffer[ret] = '\0';
-		line += buffer;
+	std::string buffer;
+
+	std::ifstream ifs(_path);
+	if  (!ifs.is_open())
+		throw Config::FileNotFoundException();
+	while (std::getline(ifs, buffer, '\n'))
+	{
+		const char *t = " \t\n\r\f\v;";
+		buffer.erase(0, buffer.find_first_not_of(t));
+		buffer.erase(buffer.find_last_not_of(t) + 1);
+		if (buffer.size() > 0)
+			file.push_back(buffer);
 	}
-
-	//	Get the last line, which is ignored otherwise
-	if (ret == -1) {
-		std::cerr << "Error: couldn't reading config file" <<std::endl;
-		return file;
-	}
-	file = ConfigReader::split(line, std::string(" \n\t"));
- 	return file;
 }
+
+void	Config::configParse()
+{
+	unsigned int	fileSize;
+
+	fileSize = file.size();
+	if (file.empty() || fileSize < 8)
+		exit (1);
+	for (std::vector<int>::size_type i = 0; i < fileSize; i++)
+	{
+		if (file[i] == "server") {
+			ConfigServer  server;
+			++i;
+			if (file[i] != "{") {
+				std::cerr << "Error: expected '{' after server directive." << std::endl;
+				exit 1;
+			}
+			++i;
+			if (!server.parseServer(i, file)) {
+				std::cerr << "Error: error in config file [" << filename << "]" ] <<  std::endl;
+				exit 1;
+			}
+			this->_servers.push_back(server);
+		}
+		else {
+				std::cerr << "Error: unknown directive [" << file[i] << "]" ] << std::endl;
+				exit 1;
+		}
+	}
+
+
+
+}
+
+
+const char	*Config::FileNotFoundException::what() const throw(){
+	return "Exception thrown: could not open configuration file";
+}
+
 #endif
--- config/main.cpp
+++ config/main.cpp
@@ -3,27 +3,34 @@
 #include <iostream>
 #include <stdexcept>
 #include <ctime>
+#include <string>
+#include <exception>
 
 Config	g_conf("default.conf");
 
 int main(int argc, char** argv) {
 	if (argc == 2) {
+		std::string path = argv[1];
+		if (!(path.compare(path.rfind('.'), 7, ".config"))) {
+			std::cerr << "Error: Config" << std::endl;
+			return (1);
+		}
 		try {
-			g_conf = Config(argv[1]);
+			g_conf = Config(path);
 		}
 		catch (std::exception& e) {
 			std::cerr << "Error: Config" << std::endl;
 			return (1);
 		}
 	}
-	try {
+	// try {
 		
-		//루프
-	}
-	catch (std::exception& e) {
-		std::cerr << "Error: Event" << std::endl;
-		return (1);
-	}
+	// 	//루프
+	// }
+	// catch (std::exception& e) {
+	// 	std::cerr << "Error: Event" << std::endl;
+	// 	return (1);
+	// }
 	
     return 0;
 }
