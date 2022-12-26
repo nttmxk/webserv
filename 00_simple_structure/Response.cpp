c07501caf30aa594422d9bcfa7ea85f290b3d8bd
whaleshade <whaleshade@gmail.com>
2022-12-26 22:26:27 +0900

    NEW : simple c++ webserver

--- 00_simple_structure/Response.cpp
+++ 00_simple_structure/Response.cpp
@@ -17,10 +17,8 @@ Response::responseToClient(int clientSocket, InfoServer &serverInfo)
 std::string
 Response::makeResponseMsg()
 {
-	std::string htmlMsg = "<!DOCTYPE html><html lang=\"en\"><body><h1> HOME </h1><p> Hello from your Server :) </p></body></html>";
+	std::string htmlMsg = "<!DOCTYPE html><html lang=\"en\"><body><h1> Webserv </h1><h2> Hello from Server </h2></body></html>";
 	std::ostringstream ss;
-	ss << "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: " << htmlMsg.size() << "\r\n"
-	   << htmlMsg;
-
+	ss << "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: " << htmlMsg.size() << "\r\n" << htmlMsg;
 	return (ss.str());
 }
